06ee6771ea5dbe3e4b6fa330df1c80bc4c93ada9
fabianaRusso <fabi.pereiraa@hotmail.com>
2023-05-24 17:27:00 -0300

    Aula 1 - Java

---
+++ src/aula1/VariaveisConstantes.java
@@ -0,0 +1,23 @@
+package aula1;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		
+		//variaveis
+		
+		int idade;
+		float altura;
+		boolean resposta;
+		String palavra = "Generation";
+		
+		
+		// Constantes
+		
+		final int Quantidade;
+		final String Turma = "Turma 65";
+		final double Area = 45.4567;
+
+	}
+
+}
